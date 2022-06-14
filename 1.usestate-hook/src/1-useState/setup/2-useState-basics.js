b1bed316d03b69c3f8fa2ae0a7494c2fe2677a7a
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-06-14 21:52:02 +0530

    Use State Completed

--- 1.usestate-hook/src/1-useState/setup/2-useState-basics.js
+++ 1.usestate-hook/src/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  console.log(useState("hello world"));
+  const [text, setText] = useState("random title");
+  const handleClick = () => {
+    const newText = text === "random title" ? "hello world" : "random title";
+    setText(newText);
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
