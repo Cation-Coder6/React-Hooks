7adab9297a5403d9c90544745f43faad725553f9
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-06-20 15:32:49 +0530

    Completed Use Context hook

--- 7.prop-drilling/src/App.js
+++ 7.prop-drilling/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./1-useState/setup/5-useState-counter";
+import Setup from "./7-prop-drilling/setup/1-prop-drilling";
 function App() {
   return (
     <div className="container">
--- 8.usecontext-hook/src/App.js
+++ 8.usecontext-hook/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./1-useState/setup/5-useState-counter";
+import Setup from "./8-useContext/setup/1-context-api";
 function App() {
   return (
     <div className="container">
