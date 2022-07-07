df5cf5e9d4c16572c7d38d2da0c6bd61558a2105
Cation-Coder6 <72982245+Cation-Coder6@users.noreply.github.com>
2022-07-08 03:09:52 +0530

    pushed code

--- 9.custom-hooks/src/App.js
+++ 9.custom-hooks/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./1-useState/setup/5-useState-counter";
+import Setup from "./9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
